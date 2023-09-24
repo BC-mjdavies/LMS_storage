6196308dd594496d20a412451775e92a33c38011
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2023-09-25 00:09:36 +0100

    Seeing if these work for LMS

---
+++ online-security/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://omAgGHcZBDqprrLtsUJzkp3ySNXZwn2L_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Online Security | Diogelwch Ar-lein"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Welcome to the tutorial session on online safety. This tutorial provides a real opportunity for students to share and discuss their online safety experience with each other and understand how the online world can impact you positive or negatively. We will explore how you can interact and communicate with people safely online.&lt;/p&gt;"
+};
